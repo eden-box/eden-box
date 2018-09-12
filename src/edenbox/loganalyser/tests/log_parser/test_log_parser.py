10c4db1b81183e099b9deb0dcca095a7e7ebb0f0
Tiago Gonçalves <tiago14_ribeiro@hotmail.com>
2018-09-12 23:12:54 +0100

    Refactor tests to use pytest

--- src/edenbox/loganalyser/tests/log_parser/test_log_parser.py
+++ src/edenbox/loganalyser/tests/log_parser/test_log_parser.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3.7
 
-import unittest
+import pytest
 from log_analyser.log_parser import LogParser
+from log_analyser.log_filter import LogFilter
 from tests.log_parser.tests_config import Config
 
 
-class TestLogParserSetup(unittest.TestCase):
+@pytest.fixture
+def default_log_filter():
+    """"
+    Returns a dummy LogFilter
+    """
+    return LogFilter()
 
-    def setUp(self):
-        self.parser = LogParser(Config.DUMMY_FILE)
 
-    def test_1(self):
-        self.assertTrue(True)
+@pytest.fixture
+def default_log_parser():
+    """"
+    Returns a dummy LogParser
+    """
+    return LogParser(Config.DUMMY_FILE, default_log_filter)
 
 
-if __name__ == '__main__':
-    unittest.main()
+class TestLogParserSetup:
+
+    def test_dummy_a(self):
+        assert default_log_parser is default_log_parser
+
+    def test_dummy_b(self):
+        assert default_log_filter is not default_log_parser
