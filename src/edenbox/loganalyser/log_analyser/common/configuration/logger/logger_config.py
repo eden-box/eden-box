7477b7bc8a709ab9a20dc82854131dfc8c56cdc8
Tiago Gonçalves <tiago14_ribeiro@hotmail.com>
2019-01-23 21:31:52 +0000

    Minor fix

--- src/edenbox/loganalyser/log_analyser/common/configuration/logger/logger_config.py
+++ src/edenbox/loganalyser/log_analyser/common/configuration/logger/logger_config.py
@@ -4,7 +4,7 @@ from pkg_resources import resource_filename
 from log_analyser.common.configuration.config import Config
 
 
-class LoggerConfig(Config):
+class __LoggerConfig(Config):
     """
     Logger configuration wrapper
     """
@@ -15,3 +15,6 @@ class LoggerConfig(Config):
 
     def logging_config(self) -> str:
         return self.get_property("log_config")
+
+
+LoggerConfig = __LoggerConfig()
