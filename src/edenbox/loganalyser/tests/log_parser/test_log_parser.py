13e4d6761c676a2f68a2f6d4e44e08c4fc401fc3
Tiago Gonçalves <tiago14_ribeiro@hotmail.com>
2019-01-30 22:23:34 +0000

    Activate exception test

--- src/edenbox/loganalyser/tests/log_parser/test_log_parser.py
+++ src/edenbox/loganalyser/tests/log_parser/test_log_parser.py
@@ -6,11 +6,6 @@ from log_analyser.log_parser import LogParser, NoLogFileException
 
 class TestLogParser:
 
-    @pytest.mark.xfail
     def test_no_file_exception(self, default_log_filter):
         with pytest.raises(NoLogFileException):
             LogParser("wrong_path.txt", default_log_filter)
-
-    def test_base_processing(self, helper):
-        # TODO complete
-        assert True
